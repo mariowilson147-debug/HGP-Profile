01f50b78559b16cd79acc0af773f2ae689df4e93
Developer <developer@example.com>
2026-05-29 19:39:07 +0300

    fix: resolve ESLint error on TS sort

--- src/components/app-views/CatalogueView.tsx
+++ src/components/app-views/CatalogueView.tsx
@@ -71,7 +71,7 @@ export default function CatalogueView({ returnPath, branchId }: { returnPath: st
               };
             });
             // Sort branch products alphabetically by name
-            branchProducts.sort((a: any, b: any) => (a.name as string).localeCompare(b.name as string));
+            branchProducts.sort((a, b) => ((a as Product).name || "").localeCompare((b as Product).name || ""));
             setProducts(branchProducts as Product[]);
           } else {
             setProducts([]);
